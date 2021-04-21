001c0e4979f78d6e91a485ddad53a046f1e891cd
= <=>
2021-04-20 21:30:04 -0700

    Create a simple map to return special roman letter for specific number

--- RomanNumerals.Kata.Tests/UnitTests.cs
+++ RomanNumerals.Kata.Tests/UnitTests.cs
@@ -15,5 +15,47 @@ namespace RomanNumerals.Kata.Tests
         {
             Assert.Equal("", RomanNumerals.IntToRoman(4000));
         }
+
+        [Fact]
+        public void ConvertOneToRoman()
+        {
+            Assert.Equal("I", RomanNumerals.IntToRoman(1));
+        }
+
+        [Fact]
+        public void ConvertFourToRoman()
+        {
+            Assert.Equal("IV", RomanNumerals.IntToRoman(4));
+        }
+
+        [Fact]
+        public void ConvertFiveToRoman()
+        {
+            Assert.Equal("V", RomanNumerals.IntToRoman(5));
+        }
+
+        [Fact]
+        public void ConvertNineToRoman()
+        {
+            Assert.Equal("IX", RomanNumerals.IntToRoman(9));
+        }
+
+        [Fact]
+        public void ConvertTenToRoman()
+        {
+            Assert.Equal("X", RomanNumerals.IntToRoman(10));
+        }
+
+        [Fact]
+        public void ConvertFortyToRoman()
+        {
+            Assert.Equal("XL", RomanNumerals.IntToRoman(40));
+        }
+
+        [Fact]
+        public void ConvertFiftyToRoman()
+        {
+            Assert.Equal("L", RomanNumerals.IntToRoman(50));
+        }
     }
 }
--- RomanNumerals.Kata/RomanNumerals.cs
+++ RomanNumerals.Kata/RomanNumerals.cs
@@ -1,4 +1,7 @@
-﻿namespace RomanNumerals.Kata
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumerals.Kata
 {
     public class RomanNumerals
     {
@@ -8,7 +11,25 @@
             {
                 return "";
             }
-            return "I";
+            StringBuilder sb = new StringBuilder();
+            SortedDictionary<int, string> map = new SortedDictionary<int, string>
+            {
+                { 1, "I" },
+                { 4, "IV" },
+                { 5, "V" },
+                { 9, "IX" },
+                { 10, "X" },
+                { 40, "XL" },
+                { 50, "L" },
+                { 90, "XC" },
+                { 100, "C" },
+                { 400, "CD" },
+                { 500, "D" },
+                { 900, "CM" },
+                { 1000, "M" },
+            };
+
+            return sb.Append(map[num]).ToString();
         }
     }
 }
