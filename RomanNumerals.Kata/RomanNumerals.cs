f1990a82e399b8d7d641470275cc8eb10aa4a0a6
= <=>
2021-04-22 21:38:53 -0700

    Refactor multiple while loop to use a map instead

--- RomanNumerals.Kata/RomanNumerals.cs
+++ RomanNumerals.Kata/RomanNumerals.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace RomanNumerals.Kata
@@ -12,66 +13,32 @@ namespace RomanNumerals.Kata
             {
                 throw new Exception("Number must be between 1 & 3999.");
             }
+
             StringBuilder sb = new StringBuilder();
-            while (num >= 1000)
+            var valueMap = new SortedDictionary<int, string>
+                               {
+                                   { 1, "I" },
+                                   { 4, "IV" },
+                                   { 5, "V" },
+                                   { 9, "IX" },
+                                   { 10, "X" },
+                                   { 40, "XL" },
+                                   { 50, "L" },
+                                   { 90, "XC" },
+                                   { 100, "C" },
+                                   { 400, "CD" },
+                                   { 500, "D" },
+                                   { 900, "CM" },
+                                   { 1000, "M" },
+                               };
+
+            foreach (var kvp in valueMap.Reverse())
             {
-                sb.Append("M");
-                num -= 1000;
-            }
-            while (num >= 900)
-            {
-                sb.Append("CM");
-                num -= 900;
-            }
-            while (num >= 500)
-            {
-                sb.Append("D");
-                num -= 500;
-            }
-            while (num >= 400)
-            {
-                sb.Append("CD");
-                num -= 400;
-            }
-            while (num >= 100)
-            {
-                sb.Append("C");
-                num -= 100;
-            }
-            while (num >= 90)
-            {
-                sb.Append("XC");
-                num -= 90;
-            }
-            while (num >= 50)
-            {
-                sb.Append("L");
-                num -= 50;
-            }
-            while (num >= 10)
-            {
-                sb.Append("X");
-                num -= 10;
-            }
-            while (num >= 9)
-            {
-                sb.Append("IX");
-                num -= 9;
-            }
-            while (num >= 5)
-            {
-                sb.Append("V");
-                num -= 5;
-            }
-            while (num >= 4)
-            {
-                sb.Append("IV");
-                num -= 4;
-            }
-            while (num >= 1)
-            {
-                sb.Append("I");
-                num -= 1;
+                while (num >= kvp.Key)
+                {
+                    num -= kvp.Key;
+                    sb.Append(kvp.Value);
+                }
             }
 
             return sb.ToString();
